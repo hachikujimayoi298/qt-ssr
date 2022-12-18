5a719e2c00f27355621c008de9cb1df6e6f51dbe
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-18 06:18:27 +0000

    Adding tarfile member sanitization to extractall()

--- utils/networking.py
+++ utils/networking.py
@@ -136,7 +136,26 @@ def init_ssr_at(path, signals):
     signals.stdout.emit('Downloading and extracting from %s ...\n' % url)
     with http.request('GET', url, preload_content=False) as resp:
         with tarfile.open(mode='r:gz', fileobj=resp) as zip_fp:
-            zip_fp.extractall(root, numeric_owner=True)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(zip_fp, root, numeric_owner="True")
 
     bin_dir = os.path.join(ssr_dir, 'src')
     bin_path = os.path.join(bin_dir, 'ss-local')
